3b3357b9b440f84bb1e65e34a2fbcab8d9b1820c
Quebin <kevin.matsuda@hks.ar>
2024-08-05 21:19:42 -0300

    email valid and readme update

--- emailValid.js
+++ emailValid.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var form = document.querySelector("form");
-    var emailInput = document.getElementById("email");
-  
-    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  
-    form.addEventListener("submit", function (event) {
-      var emailValue = emailInput.value;
-  
-      if (!emailPattern.test(emailValue)) {
-        event.preventDefault();
-        event.stopPropagation();
-        alert("Por favor, ingresa un correo electrónico válido.");
-        emailInput.classList.add("is-invalid");
-      } else {
-  
+  var form = document.querySelector("form");
+  var emailInput = document.getElementById("email");
+
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  form.addEventListener("submit", function (event) {
+    var emailValue = emailInput.value;
+
+    if (!emailPattern.test(emailValue)) {
+      event.preventDefault();
+      event.stopPropagation();
+      alert("Por favor, ingresa un correo electrónico válido.");
+      emailInput.classList.add("is-invalid");
+    } else {
+      emailInput.classList.remove("is-invalid");
+    }
+  });
+});
